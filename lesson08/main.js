f9337a99ae0d60674abc19e323940219634a83fb
takaki <cheap_trick_magic@yahoo.co.jp>
2021-01-09 19:28:53 +0900

    変数・定数・関数の正しい宣言方法

---
+++ lesson08/main.js
@@ -0,0 +1,12 @@
+const testScope = (scope) => {
+  if (scope === 'function') {
+    var functionScope = '関数スコープ内ならどこからでも参照できます'
+  } else if (scope === 'block') {
+    let blockScope = 'ブロックスコープ内でしか参照できません'
+    console.log(blockScope)
+  }
+  console.log(functionScope)
+}
+
+testScope('function')
+testScope('block')
